805bbcec328014be21d03a8e6bf1dc6a08f52f36
Sazzad Sazib <sazib66@gmail.com>
2019-03-05 22:38:39 +0600

    feat: prettier

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const env = require("dotenv").config();
 //Mongo staffs;
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 const dbName = process.env.DB_NAME;
 const port = process.env.SERVER_PORT;
-const url = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@cluster0-tosfm.mongodb.net/test?retryWrites=true";
+const url =
+    "mongodb+srv://" +
+    process.env.DB_USER +
+    ":" +
+    process.env.DB_PASS +
+    "@cluster0-tosfm.mongodb.net/test?retryWrites=true";
 
-app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 app.get("/", (req, res) => res.send({ message: "sorry no endpoint available for this root path" }));
 
-app.post('/login', function(req, res) {
+app.post("/login", function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
     let statusData = null;
-    MongoClient.connect(url,{ useNewUrlParser: true }, function(err, client) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
         assert.equal(null, err);
         // console.log("Connected successfully to server");
         const db = client.db(dbName);
-        const collection = db.collection('user');
-        collection.findOne({name: username}, (err, item) => {console.log(item)});
+        const collection = db.collection("user");
+        collection.findOne({ name: username }, (err, item) => {
+            console.log(item);
+        });
         client.close();
     });
     return res.send(statusData);
 });
-
 
 app.listen(port, () => console.log(`Running on ${port}!`));
