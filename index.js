3b80bf71e5acc197e41a92695db405e1374bc1be
Sazzad Sazib <sazib66@gmail.com>
2019-03-05 22:37:42 +0600

    feat: added some feature

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 const express = require("express");
 const app = express();
-const port = 5001;
+const bodyParser = require('body-parser');
+const env = require("dotenv").config();
 //Mongo staffs;
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
-const env = require("dotenv").config();
 const dbName = process.env.DB_NAME;
-const url =
-    "mongodb+srv://" +
-    process.env.DB_USER +
-    ":" +
-    process.env.DB_PASS +
-    "@cluster0-tosfm.mongodb.net/test?retryWrites=true";
+const port = process.env.SERVER_PORT;
+const url = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@cluster0-tosfm.mongodb.net/test?retryWrites=true";
 
-MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-    const db = client.db(dbName);
-    const collection = db.collection("user");
-    collection.find().toArray((err, items) => {
-        console.log(items);
+app.get("/", (req, res) => res.send({ message: "sorry no endpoint available for this root path" }));
+
+app.post('/login', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    let statusData = null;
+    MongoClient.connect(url,{ useNewUrlParser: true }, function(err, client) {
+        assert.equal(null, err);
+        // console.log("Connected successfully to server");
+        const db = client.db(dbName);
+        const collection = db.collection('user');
+        collection.findOne({name: username}, (err, item) => {console.log(item)});
+        client.close();
     });
-    client.close();
+    return res.send(statusData);
 });
 
-app.get("/", (req, res) =>
-    res.send({ message: "sorry no endpoint available for this root path" })
-);
-
-app.get("/userAuth", (req, res) => {
-    console.log("hi");
-    return res.send({ userAUTH: "LEL" });
-});
 
 app.listen(port, () => console.log(`Running on ${port}!`));
